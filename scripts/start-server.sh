9e18a9ea019a766d58a5fc524a5e162d972ce62a
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-02 15:02:08 +0900

    asd

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "-------------서버 시작-------------"
 docker stop github-actions || true
 docker rm github-actions || true
-docker pull 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest || { echo "❌ 이미지 다운로드 실패! 배포 중단"; exit 1; }
+docker pull 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
 docker run -d --name github-actions -p 8080:8080 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
 echo "-------------서버 배포 끝-------------"
